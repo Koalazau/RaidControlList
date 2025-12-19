ad1604c2da027a7b82ec0d107858d8f804854808
Koalazau <koalaz_au@hotmail.com>
2025-12-19 12:52:09 +1000

    Create whitelistnames.lua for player management
    Initialize whitelist for auto-locking raid.

--- whitelistnames.lua
+++ whitelistnames.lua
@@ -1,1 +1,11 @@
+-- whitelistnames.lua
+-- Whitelist for Raid Lock feature
+-- This file is updated from GitHub
 
+RaidControl = RaidControl or {}
+RaidControl.Whitelist = RaidControl.Whitelist or {}
+
+-- Add player names to this list (case-sensitive)
+RaidControl.Whitelist.names = {
+    "Koala",
+}
